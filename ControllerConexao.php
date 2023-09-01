ed96fbaa98040ac5fc98ebfd36be0f5ed11a9344
samuca23 <samuelchiodini@hotmail.com>
2023-09-01 00:55:18 -0300

    Adicionar documentação

--- ControllerConexao.php
+++ ControllerConexao.php
@@ -7,6 +7,14 @@ class ControllerConexao {
 
     public function __construct()
     {
+        $this->createConnection();
+    }
+    
+    /**
+     * Método utilizado para criar a coneção
+     */
+    private function createConnection() 
+    {
         $oConexao = new Conexao();
         $oConexao->setConexao();
         $this->conexao = $oConexao;
--- ControllerVendas.php
+++ ControllerVendas.php
@@ -16,6 +16,11 @@ require_once "ControllerConexao.php";
 class ControllerVendas extends ControllerConexao
 {
 
+    /**
+     * Método responsável por retornar o total de vendas
+     *
+     * @return integer
+     */
     public function getQuantitySale(): int
     {
         $sSql = "SELECT COALESCE(MAX(vencodigo), 0) AS total_venda
